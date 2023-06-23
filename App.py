24d0a7d836a306a800c7fa2f714c11876c5560e8
hoyinli1211 <hoyinli.r@gmail.com>
2023-06-23 23:37:18 +0800

    Create App.py

---
+++ App.py
@@ -0,0 +1,37 @@
+import requests
+import streamlit as st
+
+# Function to search for an app by name and return its app ID
+def search_app(app_name, country_code="us"):
+    url = "https://itunes.apple.com/search"
+    params = {
+        "term": app_name,
+        "country": country_code,
+        "entity": "software",
+        "limit": 1
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    results = response.json()["results"]
+    if results:
+        return results[0]["trackId"]
+    else:
+        return None
+
+# Function to fetch reviews using the RSS iTunes Reviews API
+def fetch_reviews(app_id, country_code="us", page=1):
+    url = f"https://rss.itunes.apple.com/api/v1/{country_code}/apps/{app_id}/reviews"
+    params = {"page": page}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+def main():
+    st.title("Apple Store Customer Reviews")
+
+    app_name = st.text_input("Enter the name of the app:")
+    country_code = st.text_input("Enter the country code (e.g., 'us'):", "us")
+
+    if app_name and country_code:
+        app_id = search_app(app_name, country_code)
+        if app_id:
